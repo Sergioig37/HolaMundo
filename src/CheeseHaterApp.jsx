a6f1fd8d354b4927b0495d560a20b57af850a825
Sergioig37 <sergioig37@educastur.es>
2024-01-22 13:45:54 +0100

    hook y eventos

---
+++ src/CheeseHaterApp.jsx
@@ -0,0 +1,11 @@
+
+
+export const CheeseHaterApp = () => {
+  return (
+    <div>CheeseHaterApp</div>
+  )
+}
+
+const manejadora = (evento) => {
+    if(evento.target.value.toLowerCase().includes("queso"))
+}
--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,15 +1,27 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
 
-import PropTypes from 'prop-types';
+export const CounterApp = ({ value }) => {
+  //esto es un hook, son funciones que nos permiten conectarnos al estado de React
+  const [contador, setContador] = useState(value);
 
-export const CounterApp = ({value}) => {
+
+  //El hook se puede refrescar con el setContador, es la única manera de cambiar la constante.
+  const pulsar = () => {
+    console.log("+1");
+    setContador(contador+1);
+  };
+
   return (
     <>
-        <h1>CounterApp</h1>
-        <h2>{value}</h2>
+      <h1>CounterApp</h1>
+      <h2>{contador}</h2>
+
+      <button onClick={pulsar}>+1</button>
     </>
-  )
-}
+  );
+};
 
 CounterApp.propTypes = {
-  value: PropTypes.number
-}
+  value: PropTypes.number,
+};
